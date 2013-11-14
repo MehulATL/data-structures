66372198d63da8c220644f6910ad8575e3edf4ec
Hack Reactor Students <mehul8@gmail.com>
2013-11-14 13:17:00 -0800

    finsihed psuedoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,28 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  this.storage = {};
 };
+
+var queueMethods = {};
+
+// Implement the methods below
+Queue.prototype.enqueue = function(value){
+  var keyCount = Object.keys(this.storage).length;
+  this.storage[keyCount] = value;
+};
+
+Queue.prototype.dequeue = function(){
+  var removedKey = this.storage[0];
+  delete this.storage[0];
+  for (var key in this.storage){
+    this.storage[key-1] = this.storage[key];
+    delete this.storage[key];
+  }
+  return removedKey;
+};
+
+Queue.prototype.size = function(){
+  return Object.keys(this.storage).length;
+};
+
