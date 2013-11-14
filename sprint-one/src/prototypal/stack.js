d29e2fdeead4130b2a922e0b76c966ecfd485a02
Hack Reactor Students <mehul8@gmail.com>
2013-11-14 11:17:39 -0800

    stack shared behaviors passed

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,29 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = Object.create(makeStack.stackMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.len = 0; // Hint: set an initial value here
+
+  return instance;
 };
 
+makeStack.stackMethods = {};
+
+makeStack.stackMethods.push = function(value){
+  this.len++;
+  this.storage[this.len] = value;
+};
+
+makeStack.stackMethods.pop = function(){
+  this.len && this.len--;
+  return this.storage[this.len+1];
+};
+
+makeStack.stackMethods.size = function(){
+  return this.len;
+};
 var stackMethods = {};
+
+
